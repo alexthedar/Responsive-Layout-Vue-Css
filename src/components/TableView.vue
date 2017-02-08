fa3057f09081eaaf9f176737d6ede67cc2fd228e
alexthedar <alex.castaneda@gmail.com>
2017-02-08 14:30:25 -0800

    dynamic table complete but not sortable.  TODO: make function on table that sorts data based on clicks of tab;e headers

--- src/components/TableView.vue
+++ src/components/TableView.vue
@@ -4,132 +4,37 @@
     <table class="table">
       <thead>
         <tr>
-          <th><i></i></th>
-          <th class="test"><abbr title="Name">Name<i class="fa fa-caret-up"></i></abbr></th>
+          <th></th>
+          <th ><abbr title="Name">Name<i class="fa fa-caret-up"></i></abbr></th>
           <th><abbr title="Size">Size<i class="fa fa-caret-down"></i></abbr></th>
           <th><abbr title="Date">Date<i class="fa fa-caret-down"></i></abbr></th>
-          <th><i></i></th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="item in data">
-          <th >
-            <span v-if="item.type==='folder'"><i class="fa fa-folder-o"></i></span>
-            <span v-else-if="item.type==='.doc'"><i class="fa fa-file-word-o"></i></span>
-            <span v-else-if="item.type==='.zip'"><i class="fa fa-file-archive-o"></i></span>
+          <td>
+            <span v-if="item.type==='folder'">
+              <i class="fa fa-folder-o"></i>
+            </span>
+            <span v-else-if="item.type==='.doc'">
+              <i class="fa fa-file-word-o"></i>
+            </span>
+            <span v-else-if="item.type==='.zip'">
+              <i class="fa fa-file-archive-o"></i>
+            </span>
             <span v-else="item.type==='img'">
               <figure class="image is-24x24">
                 <img class="image-cropper" :src="item.img">
               </figure>
             </span>
-          </th>
-          {{item}}
-        </tr>
-
-        <tr>
-          <th></th>
-          <td>Folder Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><label class="file-menu" for="row1"><i  class="fa fa-ellipsis-v"></i></label>
-            <input id="row1" type="checkbox" class="toggle"></input>
-
-            <div class=" toggled">
-                Text that be hidden dynamically!
-            </div>
           </td>
+          <td>{{item.name}}</td>
+          <td class="size"><small>{{item.size}}</small></td>
+          <td>{{getDate(item.date)}}</td>
+          <td><i class="fa fa-ellipsis-v"></i></td>
         </tr>
-        <tr>
-          <th><i class="fa fa-folder-o"></i></th>
-          <td>Folder Name</td>
-          <td class="size"><small>2.56mb</small></td>
-          <td>04-14-2016</td>
-          <td><a @click="show"><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-folder-o"></i></th>
-          <td>Folder Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-folder-o"></i></th>
-          <td>Folder Name</td>
-          <td class="size"><small>2.56mb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-file-word-o"></i></th>
-          <td>File Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-file-word-o"></i></th>
-          <td>File Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th>
-            <figure class="image is-24x24">
-              <img class="image-cropper" src="http://lorempixel.com/250/250/sports/">
-            </figure>
-          </th>
-          <td>File Name</td>
-          <td class="size"><small>2.56mb</small></td>
-          <td>04-14-2016</td>
-          <td><a class="file-menu"><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-file-archive-o"></i></th>
-          <td>File Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-file-word-o"></i></th>
-          <td>File Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-file-word-o"></i></th>
-          <td>File Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th>
-            <figure class="image is-24x24">
-              <img class="image-cropper" src="http://lorempixel.com/250/250/cats/">
-            </figure>
-          </th>
-          <td>File Name</td>
-          <td class="size"><small>2.56mb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-        <tr>
-          <th><i class="fa fa-file-archive-o"></i></th>
-          <td>File Name</td>
-          <td class="size"><small>128kb</small></td>
-          <td>04-14-2016</td>
-          <td><a><i class="fa fa-ellipsis-v"></i></a></td>
-        </tr>
-      </tbody>
-    </table>
-    <div class="box" >
-    <p>One</p>
-    <span><a href="#top">Close</a></span>
-</div>
+
   </div>
 
 </template>
@@ -142,6 +47,7 @@
 
 <script>
 import data from '../assets/json/data.json'
+import moment from 'moment'
 // import Menu from './components/Menu'
 // import MainBody from './components/MainBody'
 
@@ -158,6 +64,9 @@ export default {
   methods: {
     show (){
       console.log(this.data)
+    },
+    getDate (date){
+      return moment.utc(date).format('MM-DD-YYYY')
     }
   }
 }
